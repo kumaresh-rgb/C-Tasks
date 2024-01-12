bfac9d6f4df53effc5a5a56c44efd6d93f1747a4
M kumaresh <83907610+kumaresh-rgb@users.noreply.github.com>
2024-01-12 19:04:24 +0530

    Update Task1-version1.0x.cs

--- Task1-version1.0x.cs
+++ Task1-version1.0x.cs
@@ -5,16 +5,16 @@ namespace MyNamespace
 {
     public class SampleClass
     {
-        
+
 
         public static void Main()
         {
             List<string> Roles = new List<String> { "Manager", "Developer", "IT Support ", "Marketing", "senior Developer" };
-            
+
             string UserEnteredRoles;
             int UserEnterPosition;
-            bool ValidorNot=false;
-            
+            bool ValidorNot = false;
+
 
             try
             {
@@ -23,7 +23,7 @@ namespace MyNamespace
                     Console.WriteLine("Enter your Role");
                     UserEnteredRoles = Console.ReadLine().Trim();
                     Console.WriteLine("Enter Your Position You Want");
-                    UserEnterPosition = int.Parse(Console.ReadLine()) ;
+                    UserEnterPosition = int.Parse(Console.ReadLine());
                 }
 
 
@@ -31,23 +31,24 @@ namespace MyNamespace
                 {
                     try
                     {
-                       
-                            if (Roles.Contains(UserEnteredRoles))
-                            {
-                                   Roles.Remove(UserEnteredRoles);
-Roles.Insert(UserEnterPosition,UserEnteredRoles);
-                                ValidorNot = true;
 
-                            }
-                            else
-                            {
-                                Console.WriteLine($"Your Role : {UserEnteredRoles} Not present in Array");
-                            }
-                        
-               
-                      
+                        if (Roles.Contains(UserEnteredRoles))
+                        {
+                            Roles.Remove(UserEnteredRoles);
+                            Roles.Insert(UserEnterPosition, UserEnteredRoles);
+                            ValidorNot = true;
+
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Your Role : {UserEnteredRoles} Not present in Array");
+                        }
+
+
+
                     }
-                    catch (Exception ex) {
+                    catch (Exception ex)
+                    {
                         Console.WriteLine("Error: {0}", ex.Message);
                         Console.WriteLine($"You Entered {UserEnterPosition} Wrong Postion Array Length Our Max Array Length Is: {Roles.Count}");
                     }
@@ -58,17 +59,17 @@ Roles.Insert(UserEnterPosition,UserEnteredRoles);
                 {
                     Console.WriteLine($"you Added this role {UserEnteredRoles} at this Position: {UserEnterPosition}");
 
-                    
-                        //   Console.WriteLine($"Total Roles are :{Roles[i]} Toltal Length {Roles.Length}");
-                        for (int j = 0; j < Roles.Count; j++)
-                        {
-                            Console.WriteLine($"Total Roles Position are :{j} Roles Are :{Roles[j]}");
-                        }
-                       
-                    
+
+                    //   Console.WriteLine($"Total Roles are :{Roles[i]} Toltal Length {Roles.Length}");
+                    for (int j = 0; j < Roles.Count; j++)
+                    {
+                        Console.WriteLine($"Total Roles Position are :{j} Roles Are :{Roles[j]}");
+                    }
+
+
                 }
 
-             
+
 
             }
 
